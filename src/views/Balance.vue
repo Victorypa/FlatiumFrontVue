6bca4db86e23d9ee17f4ab9c2e91f5932f0fab6c
victorypa <victorypa1992@yandex.ru>
2018-10-26 13:16:55 +0300

    balanceWithDatepicker

--- src/views/Balance.vue
+++ src/views/Balance.vue
@@ -33,7 +33,7 @@
             <div class="col-12">
   
               <div class="main-content">
-                <div class="row">
+                <div class="row align-items-center">
                   <div class="main-subtitle profit col-1 mr-4">
                     Приход:
                   </div>
@@ -42,19 +42,18 @@
                   </div>
                   <div class="col-2">
                     <select class="form-control">
-                                <option selected disabled>Причина</option>
-                                <option>1</option>
-                                <option>1</option>
-                                <option>1</option>
-                              </select>
+                                          <option selected disabled>Причина</option>
+                                          <option>1</option>
+                                          <option>1</option>
+                                          <option>1</option>
+                                        </select>
                   </div>
                   <div class="col-2">
-                    <input type="text" class="form-control" placeholder="Дата">
+                    <datepicker class="my-datepicker" :language="ru"></datepicker>
                   </div>
-  
                 </div>
   
-                <div class="row py-4">
+                <div class="row align-items-center py-4">
                   <div class="main-subtitle profit col-1 mr-4">
                     Расход:
                   </div>
@@ -63,14 +62,14 @@
                   </div>
                   <div class="col-2">
                     <select class="form-control">
-                                <option selected disabled>Причина</option>
-                                <option>1</option>
-                                <option>1</option>
-                                <option>1</option>
-                              </select>
+                                          <option selected disabled>Причина</option>
+                                          <option>1</option>
+                                          <option>1</option>
+                                          <option>1</option>
+                                        </select>
                   </div>
                   <div class="col-2">
-                    <input type="text" class="form-control" placeholder="Дата">
+                    <datepicker class="my-datepicker" :language="ru"></datepicker>
                   </div>
   
                 </div>
@@ -88,44 +87,44 @@
                     <td>Оплата материалов</td>
                     <td>10 октября 2018</td>
                     <td> <button class="add-button add-button--remove d-flex align-items-center" title="Удалить материал">
-                        <img src="../assets/img/del.svg" alt="add-button">
-                        <div class="remove-materials ml-1">
-                          Удалить
-                        </div>
-                      </button></td>
+                                  <img src="../assets/img/del.svg" alt="add-button">
+                                  <div class="remove-materials ml-1">
+                                    Удалить
+                                  </div>
+                                </button></td>
                   </tr>
                   <tr>
-                   <td class="pl-4">+1 200 000</td>
+                    <td class="pl-4">+1 200 000</td>
                     <td>Акт №1</td>
                     <td>10 октября 2018</td>
                     <td> <button class="add-button add-button--remove d-flex align-items-center" title="Удалить материал">
-                        <img src="../assets/img/del.svg" alt="add-button">
-                        <div class="remove-materials ml-1">
-                          Удалить
-                        </div>
-                      </button></td>
+                                  <img src="../assets/img/del.svg" alt="add-button">
+                                  <div class="remove-materials ml-1">
+                                    Удалить
+                                  </div>
+                                </button></td>
                   </tr>
                   <tr>
-                   <td class="pl-4">+1 200 000</td>
+                    <td class="pl-4">+1 200 000</td>
                     <td>Оплата материалов</td>
                     <td>10 октября 2018</td>
                     <td> <button class="add-button add-button--remove d-flex align-items-center" title="Удалить материал">
-                        <img src="../assets/img/del.svg" alt="add-button">
-                        <div class="remove-materials ml-1">
-                          Удалить
-                        </div>
-                      </button></td>
+                                  <img src="../assets/img/del.svg" alt="add-button">
+                                  <div class="remove-materials ml-1">
+                                    Удалить
+                                  </div>
+                                </button></td>
                   </tr>
                   <tr>
                     <td class="pl-4">+1 200 000</td>
                     <td>Оплата рабочим</td>
                     <td>10 октября 2018</td>
                     <td> <button class="add-button add-button--remove d-flex align-items-center" title="Удалить материал">
-                        <img src="../assets/img/del.svg" alt="add-button">
-                        <div class="remove-materials ml-1">
-                          Удалить
-                        </div>
-                      </button></td>
+                                  <img src="../assets/img/del.svg" alt="add-button">
+                                  <div class="remove-materials ml-1">
+                                    Удалить
+                                  </div>
+                                </button></td>
                   </tr>
                 </tbody>
               </table>
@@ -140,66 +139,73 @@
 
 
 <script>
-  import basicheader from "./partials/BasicHeader";
-  import sidebar from "./partials/Sidebar";
-  export default {
-    components: {
-      basicheader,
-      sidebar
-    }
-  };
+import basicheader from "./partials/BasicHeader";
+import sidebar from "./partials/Sidebar";
+import Datepicker from "vuejs-datepicker";
+import { ru } from "vuejs-datepicker/dist/locale";
+export default {
+  components: {
+    basicheader,
+    sidebar,
+    Datepicker
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  @import "../assets/scss/vars.scss";
-  .fixed-part {
-    position: fixed;
-    background-color: $white;
-    padding-bottom: 35px;
-    padding-top: 85px;
-    z-index: 999;
+@import "../assets/scss/vars.scss";
+.fixed-part {
+  position: fixed;
+  background-color: $white;
+  padding-bottom: 35px;
+  padding-top: 85px;
+  z-index: 999;
+}
+
+.sidebar {
+  min-height: 100vh;
+}
+
+.main-content {
+  padding-top: 275px;
+}
+
+.add-button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  &:focus {
+    outline: none;
   }
-  
-  .sidebar {
-    min-height: 100vh;
+  img {
+    width: 35px;
+    border-radius: 50%;
+    @include transition;
+    &:hover {
+      box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
+    }
   }
-  
-  .main-content {
-    padding-top: 275px;
-  }
-  
-  .add-button {
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
-    &:focus {
-      outline: none;
+  &--remove {
+    color: #ccc;
+    &:hover {
+      color: $main-color;
     }
     img {
-      width: 35px;
-      border-radius: 50%;
-      @include transition;
-      &:hover {
-        box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
-      }
-    }
-    &--remove {
-      color: #ccc;
-      &:hover {
-        color: $main-color;
-      }
-      img {
-        width: 15px;
-      }
+      width: 15px;
     }
   }
-  
-  .remove-materials {
-    font-size: 0.75rem;
-  }
+}
 
-  .bg {
-    padding-bottom: 400px;
-  }
+.remove-materials {
+  font-size: 0.75rem;
+}
+
+.bg {
+  padding-bottom: 400px;
+}
+
+.my-datepicker {
+  line-height: 15px;
+}
 </style>
 
